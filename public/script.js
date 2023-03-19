1143fcd08b72c28ad23ba4d569bf6de89b27199a
kafahchaa <kafahcha@gmail.com>
2023-03-19 17:41:20 +0700

    TASK MODUL 3

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 // api url
-const api_url = "https://api.thingspeak.com/channels/<ChannelID>/feeds.json?api_key=<ReadAPIKeys>&results=1";
+const api_url = "https://api.thingspeak.com/channels/2071451/feeds.json?api_key=XVFWWT4GBQE7PWU6&results=1";
 // masukan url sesuai dengan format <ChannelID>, <ReadAPIKeys> yang ada di Thingspeak, tanpa menggunakan < >
 
 setInterval(() => {
@@ -8,7 +8,8 @@ setInterval(() => {
     .then((res) => {
       var field = JSON.stringify(res.feeds[0]);
       var obj = JSON.parse(field);
-      document.getElementById("field-1").innerHTML = obj.field1;
-      document.getElementById("field-2").innerHTML = obj.field2;
+      document.getElementById("PirState").innerHTML = obj.field1;
+      document.getElementById("Temperature").innerHTML = obj.field2;
+      document.getElementById("Kelembapan").innerHTML = obj.field3;
     });
 }, 1000);
